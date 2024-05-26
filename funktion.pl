04f4e062fa41c566eb0d485d58e6d88be1efd765
dolmario <dolmario@web.de>
2024-05-26 21:29:15 +0200

    Delete funktion.pl

--- funktion.pl
+++
@@ -1,2 +0,0 @@
-
-Dieser Vorschlag integriert die zusätzlichen Informationen nahtlos in die README-Datei und gibt den Benutzern einen klaren Überblick über die Funktionen und die Verwendung des Chatbots.

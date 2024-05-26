4c5aaf77db65f587bc2f2f2c60bc47334ccc6824
dolmario <dolmario@web.de>
2024-05-26 21:05:10 +0200

    Create funktion.pl

---
+++ funktion.pl
@@ -0,0 +1,2 @@
+
+Dieser Vorschlag integriert die zusätzlichen Informationen nahtlos in die README-Datei und gibt den Benutzern einen klaren Überblick über die Funktionen und die Verwendung des Chatbots.
